ab7a660f60013cabe287b8d46478d82b445fa209
Jason Dillon <jason@planet57.com>
2014-01-13 15:29:07 -0800

    fix classname

--- plugins/rapture/nexus-coreui-plugin/src/main/resources/static/rapture/NX/coreui/view/RepositoryList.js
+++ plugins/rapture/nexus-coreui-plugin/src/main/resources/static/rapture/NX/coreui/view/RepositoryList.js
@@ -10,7 +10,7 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
-Ext.define('NX.coreui.view.List', {
+Ext.define('NX.coreui.view.RepositoryList', {
   extend: 'Ext.grid.Panel',
   alias: 'widget.nx-repository-list',
 
