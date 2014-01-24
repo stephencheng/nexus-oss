6c6d67f3c055d426d791ddb2e59845990d93b568
Alin Dreghiciu <adreghiciu@gmail.com>
2014-01-24 23:43:05 +0200

    Modify Add Ext.ux.ActivityMonitor to not be a singleton / drop need to init

--- plugins/rapture/nexus-rapture-plugin/src/main/resources/static/rapture/Ext/ux/ActivityMonitor.js
+++ plugins/rapture/nexus-rapture-plugin/src/main/resources/static/rapture/Ext/ux/ActivityMonitor.js
@@ -1,4 +1,3 @@
-
 /**
  * @class Ext.ux.ActivityMonitor
  * @author Arthur Kay (http://www.akawebdesign.com)
@@ -31,48 +30,41 @@
  *
  */
 Ext.define('Ext.ux.ActivityMonitor', {
-  singleton   : true,
 
-  ui          : null,
-  runner      : null,
-  task        : null,
-  lastActive  : null,
+  ui: null,
+  runner: null,
+  task: null,
+  lastActive: null,
 
-  ready       : false,
-  verbose     : false,
-  interval    : (1000 * 60 * 1), //1 minute
-  maxInactive : (1000 * 60 * 5), //5 minutes
+  ready: false,
+  verbose: false,
+  interval: (1000 * 60 * 1), //1 minute
+  maxInactive: (1000 * 60 * 5), //5 minutes
 
-  init : function(config) {
-    if (!config) { config = {}; }
+  constructor: function (config) {
+    if (!config) {
+      config = {};
+    }
 
     Ext.apply(this, config, {
-      runner     : new Ext.util.TaskRunner(),
-      ui         : Ext.getBody(),
-      task       : {
-        run      : this.monitorUI,
-        interval : config.interval || this.interval,
-        scope    : this
+      runner: new Ext.util.TaskRunner(),
+      ui: Ext.getBody(),
+      task: {
+        run: this.monitorUI,
+        interval: config.interval || this.interval,
+        scope: this
       }
     });
 
-    this.ready = true;
+    this.callParent(arguments);
   },
 
-  isReady : function() {
-    return this.ready;
-  },
+  isActive: Ext.emptyFn,
+  isInactive: Ext.emptyFn,
 
-  isActive   : Ext.emptyFn,
-  isInactive : Ext.emptyFn,
-
-  start : function() {
-    if (!this.isReady()) {
-      this.log('Please run ActivityMonitor.init()');
-      return false;
-    }
-
+  start: function () {
     this.ui.on('mousemove', this.captureActivity, this);
+    this.ui.on('mousedown', this.captureActivity, this);
     this.ui.on('keydown', this.captureActivity, this);
 
     this.lastActive = new Date();
@@ -81,27 +73,23 @@ Ext.define('Ext.ux.ActivityMonitor', {
     this.runner.start(this.task);
   },
 
-  stop : function() {
-    if (!this.isReady()) {
-      this.log('Please run ActivityMonitor.init()');
-      return false;
-    }
-
+  stop: function () {
     this.runner.stop(this.task);
     this.lastActive = null;
 
     this.ui.un('mousemove', this.captureActivity);
+    this.ui.un('mousedown', this.captureActivity, this);
     this.ui.un('keydown', this.captureActivity);
 
     this.log('ActivityMonitor has been stopped.');
   },
 
-  captureActivity : function(eventObj, el, eventOptions) {
+  captureActivity: function (eventObj, el, eventOptions) {
     this.lastActive = new Date();
   },
 
-  monitorUI : function() {
-    var now      = new Date(),
+  monitorUI: function () {
+    var now = new Date(),
         inactive = (now - this.lastActive);
 
     if (inactive >= this.maxInactive) {
@@ -116,7 +104,7 @@ Ext.define('Ext.ux.ActivityMonitor', {
     }
   },
 
-  log : function(msg) {
+  log: function (msg) {
     if (this.verbose) {
       window.console.log(msg);
     }
