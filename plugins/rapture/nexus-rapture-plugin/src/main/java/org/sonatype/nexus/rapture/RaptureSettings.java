a133e73cd17b8ea8baa54904ff8d6364cbdd7681
Alin Dreghiciu <adreghiciu@gmail.com>
2014-01-26 14:48:56 +0200

    Add rapture service (for now just settings)

---
+++ plugins/rapture/nexus-rapture-plugin/src/main/java/org/sonatype/nexus/rapture/RaptureSettings.java
@@ -0,0 +1,52 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2007-2013 Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+
+package org.sonatype.nexus.rapture;
+
+import org.sonatype.sisu.goodies.common.ComponentSupport;
+
+/**
+ * RaptureSettings.
+ *
+ * @since 2.8
+ */
+public class RaptureSettings
+    extends ComponentSupport
+{
+
+  public static final boolean DEFAULT_DEBUG_ENABLED = true;
+
+  public static final int DEFAULT_SESSION_TIMEOUT = 30;
+
+  private boolean debugEnabled = DEFAULT_DEBUG_ENABLED;
+
+  private int sessionTimeout = DEFAULT_SESSION_TIMEOUT;
+
+
+  public boolean isDebugEnabled() {
+    return debugEnabled;
+  }
+
+  public void setDebugEnabled(final boolean debugEnabled) {
+    this.debugEnabled = debugEnabled;
+  }
+
+  public int getSessionTimeout() {
+    return sessionTimeout;
+  }
+
+  public void setSessionTimeout(final int sessionTimeout) {
+    this.sessionTimeout = sessionTimeout;
+  }
+
+}
