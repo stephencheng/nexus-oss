c6dedbbcb15eacecfafd7722e7d045232d8c3c99
Jason Dillon <jason@planet57.com>
2014-01-28 17:57:44 -0800

    docs

--- plugins/rapture/nexus-rapture-plugin/src/main/resources/static/rapture/NX/LogAware.js
+++ plugins/rapture/nexus-rapture-plugin/src/main/resources/static/rapture/NX/LogAware.js
@@ -10,6 +10,12 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
+
+/**
+ * Adds logging support helpers to objects.
+ *
+ * @since 2.8
+ */
 Ext.define('NX.LogAware', {
   requires: [
     'NX.Log'
