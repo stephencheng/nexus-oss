b2c98710db28a7fefbd4af2cf097bd0c0906d8d8
Jason Dillon <jason@planet57.com>
2014-01-31 16:39:18 -0800

    drop button glphy tests, buttons panel shows these now

--- plugins/rapture/nexus-rapture-plugin/src/main/resources/static/rapture/NX/view/dev/Tests.js
+++ plugins/rapture/nexus-rapture-plugin/src/main/resources/static/rapture/NX/view/dev/Tests.js
@@ -27,8 +27,6 @@ Ext.define('NX.view.dev.Tests', {
     { xtype: 'button', text: 'ext error', action: 'testExtError' },
     { xtype: 'button', text: 'message types', action: 'testMessages' },
     { xtype: 'button', text: 'button icon', iconCls: 'nx-icon-refresh-x16' },
-    { xtype: 'button', text: 'button glyph', glyph: 'xf130@FontAwesome' },
-    { xtype: 'button', text: 'button glyph plain', glyph: 'xf0a6@FontAwesome', ui: 'plain' },
     { xtype: 'button', text: 'toggle branding', action: 'toggleBranding' }
   ]
 });
