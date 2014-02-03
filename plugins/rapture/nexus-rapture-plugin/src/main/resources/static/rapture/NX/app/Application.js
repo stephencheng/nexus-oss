e8f1a5dca5473d16d1e4e9fa2012621298220075
Alin Dreghiciu <adreghiciu@gmail.com>
2014-02-03 19:28:15 +0200

    Add DirectUpdate form action to circumvent DirectSubmit bugs

--- plugins/rapture/nexus-rapture-plugin/src/main/resources/static/rapture/NX/app/Application.js
+++ plugins/rapture/nexus-rapture-plugin/src/main/resources/static/rapture/NX/app/Application.js
@@ -31,7 +31,8 @@ Ext.define('NX.app.Application', {
   uses: [
     'NX.ext.grid.plugin.FilterBox',
     'NX.ext.grid.plugin.Filtering',
-    'NX.ext.form.field.ValueSet'
+    'NX.ext.form.field.ValueSet',
+    'NX.ext.form.action.DirectUpdate'
   ],
 
   mixins: {
