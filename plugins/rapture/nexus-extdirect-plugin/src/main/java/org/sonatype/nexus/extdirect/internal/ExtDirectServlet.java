5b291a33c2e12f97dc96077959a3743083d28a8b
Alin Dreghiciu <adreghiciu@gmail.com>
2014-02-09 15:28:07 +0200

    Fix getting of request parameters in case that parameters were already read from request

--- plugins/rapture/nexus-extdirect-plugin/src/main/java/org/sonatype/nexus/extdirect/internal/ExtDirectServlet.java
+++ plugins/rapture/nexus-extdirect-plugin/src/main/java/org/sonatype/nexus/extdirect/internal/ExtDirectServlet.java
@@ -17,15 +17,19 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.Writer;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Nullable;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
@@ -38,11 +42,17 @@ import org.sonatype.nexus.extdirect.model.Response;
 import com.google.common.base.Function;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Maps.EntryTransformer;
 import com.google.inject.Key;
 import com.softwarementors.extjs.djn.EncodingUtils;
 import com.softwarementors.extjs.djn.api.RegisteredMethod;
+import com.softwarementors.extjs.djn.api.Registry;
 import com.softwarementors.extjs.djn.config.ApiConfiguration;
+import com.softwarementors.extjs.djn.config.GlobalConfiguration;
+import com.softwarementors.extjs.djn.router.RequestRouter;
 import com.softwarementors.extjs.djn.router.dispatcher.Dispatcher;
+import com.softwarementors.extjs.djn.router.processor.poll.PollRequestProcessor;
 import com.softwarementors.extjs.djn.servlet.DirectJNgineServlet;
 import com.softwarementors.extjs.djn.servlet.ssm.SsmDispatcher;
 import org.eclipse.sisu.BeanEntry;
@@ -84,9 +94,21 @@ public class ExtDirectServlet
   public void doPost(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
     HttpServletRequest wrappedRequest = new HttpServletRequestWrapper(request)
     {
+      private BufferedReader reader;
+
       @Override
       public BufferedReader getReader() throws IOException {
-        return new BufferedReader(new InputStreamReader(getInputStream(), EncodingUtils.UTF8));
+        if (reader == null) {
+          try {
+            reader = super.getReader();
+          }
+          catch (IllegalStateException e) {
+            // HACK avoid "java.lang.IllegalStateException: STREAMED" which is thrown in case of request.getReader()
+            // and request.getInputStream() was already called as in case of getting request parameters
+            reader = new RequestBoundReader(new InputStreamReader(getInputStream(), EncodingUtils.UTF8), request);
+          }
+        }
+        return reader;
       }
     };
     super.doPost(wrappedRequest, response);
@@ -182,4 +204,56 @@ public class ExtDirectServlet
     };
   }
 
+  @Override
+  protected RequestRouter createRequestRouter(final Registry registry, final GlobalConfiguration globalConfiguration) {
+    final Dispatcher dispatcher = createDispatcher(globalConfiguration.getDispatcherClass());
+    return new RequestRouter(registry, globalConfiguration, dispatcher)
+    {
+      @Override
+      public void processPollRequest(final Reader reader, final Writer writer, final String pathInfo)
+          throws IOException
+      {
+        new PollRequestProcessor(registry, dispatcher, globalConfiguration)
+        {
+          @Override
+          // HACK: we determine parameters from request not by reading request content as request content could had
+          // been already read exactly for getting the params, case when request content is already empty
+          protected Object[] getParameters()
+          {
+            if (reader instanceof RequestBoundReader) {
+              ServletRequest request = ((RequestBoundReader) reader).getRequest();
+              Map<String, String[]> parameterMap = request.getParameterMap();
+              Map<String, String> parameters = Maps.newHashMap();
+              if (parameterMap != null) {
+                parameters = Maps.transformEntries(parameterMap, new EntryTransformer<String, String[], String>()
+                {
+                  @Override
+                  public String transformEntry(@Nullable final String key, @Nullable final String[] values) {
+                    return values == null || values.length == 0 ? null : values[0];
+                  }
+                });
+              }
+              return new Object[]{parameters};
+            }
+            return super.getParameters();
+          }
+        }.process(reader, writer, pathInfo);
+      }
+    };
+  }
+
+  private static class RequestBoundReader
+      extends BufferedReader
+  {
+    private final ServletRequest request;
+
+    public RequestBoundReader(final Reader in, final ServletRequest request) {
+      super(in);
+      this.request = request;
+    }
+
+    private ServletRequest getRequest() {
+      return request;
+    }
+  }
 }
